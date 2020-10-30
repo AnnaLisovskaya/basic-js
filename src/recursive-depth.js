0ed16e5292877c52e370733fd4fd7ffea2492903
Anna Kovalevskaya <lao7777@ya.ru>
2020-10-30 20:59:10 +0300

    feat: recursive-deth check

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,4 +1,4 @@
-const CustomError = require("../extensions/custom-error");
+/*const CustomError = require("../extensions/custom-error");
 let i=1;
 let newArr =[];
 module.exports = class DepthCalculator {
@@ -6,7 +6,15 @@ module.exports = class DepthCalculator {
     newArr=arr.flat();
     if (JSON.stringify(newArr) === JSON.stringify(arr)){
       return i;
-    }   else i=i+1;
+    }   else {i=i+1;}
     return this.calculateDepth(arr,i);
+  }
+};*/
+const CustomError = require("../extensions/custom-error");
+
+module.exports = class DepthCalculator {
+  calculateDepth(/* arr */) {
+    throw new CustomError('Not implemented');
+    // remove line with error and write your code here
   }
 };
