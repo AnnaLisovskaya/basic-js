1bcbcf3a2f659e4193f0850d4154d3867f03eb71
Anna Kovalevskaya <lao7777@ya.ru>
2020-10-30 20:50:09 +0300

    init: start what-season, recursive-depth

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,8 +1,12 @@
 const CustomError = require("../extensions/custom-error");
-
+let i=1;
+let newArr =[];
 module.exports = class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+ calculateDepth(arr) {
+    newArr=arr.flat();
+    if (JSON.stringify(newArr) === JSON.stringify(arr)){
+      return i;
+    }   else i=i+1;
+    return this.calculateDepth(arr,i);
   }
 };
