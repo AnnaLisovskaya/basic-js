4fb721f781d9ff87de16b67eda24e6ed4a6eefbb
Anna Kovalevskaya <lao7777@ya.ru>
2020-10-31 18:22:36 +0300

    init: start dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,17 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function createDreamTeam(members) {
+  let result='',
+  i;
+  if (members.length === 0){
+    return false;
+  }
+ for (i=0; i<members.length;i++) {
+   if (typeof members[i] === 'string'){
+     result+=members[i].trim().charAt(0).toUpperCase();
+   }
+ }
+  
+ 
+return result.split('').sort().join('');
+}
