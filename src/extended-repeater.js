27ca77f864137fb7575f69473a430bb800c4abe0
AnnaKowalewskaja <60646192+AnnaKowalewskaja@users.noreply.github.com>
2021-03-29 23:07:34 +0300

    fix: repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,46 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function repeater( str, options ) {
+   
+    resFrRep=firstRep(str,options);
+
+    console.log(resFrRep);
+    
+    
+          }
+    
+          const firstRep = (str, options) => {
+            let RT = options.repeatTimes,
+                S = options.separator,
+                A = options.addition,
+                ART =options.additionRepeatTimes,
+                AS = options.additionSeparator,
+                res = str;
+    
+            if (typeof RT === "undefined" || RT == 1) {
+              return res;
+            } else {
+              while (ART>1){
+                  ART--;
+                  res=res+A+AS;
+                  
+              };
+              let fin=res;
+              while(RT>0){
+                  if (RT !=1){
+                      
+                      fin=fin+A+S+res;
+                  }else {
+                      fin+=A;
+                  }
+                  RT--;
+              }
+              return fin;
+            }
+          };
+
+
+
+
+
   
